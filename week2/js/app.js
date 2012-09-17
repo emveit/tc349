7b7615ff4d60bd689b0b5920e3db7854a3a53574
Veit <e.veit@tsc-423.techsmith.com>
2012-09-17 11:12:54 -0400

    Hello

---
+++ week2/js/app.js
@@ -0,0 +1,6 @@
+
+function hello() {
+	alert ("Browser CodeName: " + navigator.appCodeName + "\n"
+			+ "Browser Name: " + navigator.appName + "\n"
+			+ "Browser Version: " + navigator.appVersion);
+}
